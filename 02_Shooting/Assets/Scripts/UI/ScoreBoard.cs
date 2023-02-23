6c2dcbcbe3b0621e1a2831b609cff34ad7e0a269
go2665 <77224762+go2665@users.noreply.github.com>
2023-02-23 13:45:07 +0900

    2D Shooting - Score 순차적으로 갱신 기능 추가

--- 02_Shooting/Assets/Scripts/UI/ScoreBoard.cs
+++ 02_Shooting/Assets/Scripts/UI/ScoreBoard.cs
@@ -5,6 +5,24 @@ using TMPro;
 
 public class ScoreBoard : MonoBehaviour
 {
+    /// <summary>
+    /// 점수가 올라가는 속도
+    /// </summary>
+    public float scoreUpSpeed = 50.0f;
+
+    /// <summary>
+    /// 현재 UI에서 보이는 점수
+    /// </summary>
+    float currentScore = 0;
+
+    /// <summary>
+    /// 실제 플레이어가 가지고 있는 점수
+    /// </summary>
+    int targetScore = 0;
+
+    /// <summary>
+    /// 점수를 출력할 UI text
+    /// </summary>
     TextMeshProUGUI score;
 
     private void Awake()
@@ -15,20 +33,30 @@ public class ScoreBoard : MonoBehaviour
 
     private void Start()
     {
-        //score.text = "점수점수";
-        Player player = FindObjectOfType<Player>();
-
+        Player player = FindObjectOfType<Player>();     // 임시로 플레이어 가져오기
         // player의 onScoreChange 델리게이트가 실행될 때 RefreshScore를 실행해라
-        player.onScoreChange += RefreshScore;       
+        player.onScoreChange += ScoreUpdate;
+
+        score.text = currentScore.ToString();   // 초기값 설정
+    }
+
+    private void Update()
+    {
+        if( currentScore < targetScore )        // currentScore가 targetScore보다 작으면
+        {
+            // currentScore를 초당 scoreUpSpeed씩 증가시킨다.
+            currentScore += Time.deltaTime * scoreUpSpeed;
+            score.text = $"{currentScore:f0}";  // UI에 출력할 때 소수점은 0개만 출력한다.(소수점 출력안함)
+        }
     }
 
     /// <summary>
-    /// 새 점수로 UI 변경
+    /// 목표 점수를 새 점수로 설정
     /// </summary>
     /// <param name="newScore">새 점수</param>
-    void RefreshScore(int newScore)
+    void ScoreUpdate(int newScore)
     {
-        score.text = newScore.ToString();
+        targetScore = newScore;
     }
 
 }
