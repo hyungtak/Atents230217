25715eecafb696d395f0e36986a9b706e244be88
go2665 <77224762+go2665@users.noreply.github.com>
2023-03-08 12:39:18 +0900

    2D Shooting - 특수적 스포너 추가 + 스포너 리팩토링

--- 02_Shooting/Assets/Scripts/Common/Spawner/SpawnerEnemy.cs
+++ 02_Shooting/Assets/Scripts/Common/Spawner/SpawnerEnemy.cs
@@ -4,26 +4,17 @@ using UnityEngine;
 
 public class SpawnerEnemy : Spawner
 {    
-    /// <summary>
-    /// 오브젝트를 주기적으로 생성하는 코루틴
-    /// </summary>
-    /// <returns></returns>
-    override protected IEnumerator Spawn()
+    protected override void OnSpawn(EnemyBase enemy)
     {
-        while (true)     // 무한 반복(무한루프)
+        Fighter fighter = enemy as Fighter;
+        if (fighter != null)
         {
-            // 생성하고 생성한 오브젝트를 스포너의 자식으로 만들기
-            GameObject obj = Factory.Inst.GetObject(objectType);
-
-            Fighter enemy = obj.GetComponent<Fighter>();    // 생성한 게임오브젝트에서 Enemy 컴포넌트 가져오기
-            enemy.TargetPlayer = player;                // Enemy에 플레이어 설정
-
-            enemy.transform.position = transform.position;  // 스포너 위치로 이동
             float r = Random.Range(minY, maxY);             // 랜덤하게 적용할 기준 높이 구하고
-            enemy.BaseY = transform.position.y + r;         // 기준 높이 적용
-
-            //yield return wait;
-            yield return new WaitForSeconds(interval);  // 인터벌만큼 대기
+            fighter.BaseY = transform.position.y + r;         // 기준 높이 적용
+        }
+        else
+        {
+            Debug.LogError("SpawnerEnemy : 적 비행기가 아닌데 스폰하려고 함");
         }
     }
 }
--- 02_Shooting/Assets/Scripts/Common/Spawner/SpawnerSpecialEnemy.cs
+++ 02_Shooting/Assets/Scripts/Common/Spawner/SpawnerSpecialEnemy.cs
@@ -3,28 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnerSpecialEnemy : Spawner
-{    
-    /// <summary>
-    /// 오브젝트를 주기적으로 생성하는 코루틴
-    /// </summary>
-    /// <returns></returns>
-    override protected IEnumerator Spawn()
-    {
-        while (true)     // 무한 반복(무한루프)
-        {
-            // 생성하고 생성한 오브젝트를 스포너의 자식으로 만들기
-            GameObject obj = Factory.Inst.GetObject(objectType);
-
-            // 생성한 게임오브젝트에서 EnemyBase 컴포넌트 가져오기
-            EnemyBase enemy = obj.GetComponent<EnemyBase>();    
-            enemy.TargetPlayer = player;                    // Enemy에 플레이어 설정
-
-            enemy.transform.position = transform.position;  // 스포너 위치로 이동
-            float r = Random.Range(minY, maxY);             // 랜덤하게 적용할 기준 높이 구하고
-            enemy.transform.Translate(Vector3.up * r);      // 랜덤하게 높이 적용하기
-
-            //yield return wait;
-            yield return new WaitForSeconds(interval);  // 인터벌만큼 대기
-        }
-    }
+{
+    // 합치는 상황에서 다 필요없어진 상황이라 껍대기만 남음
+    // 궂이 제거할 필요가 없어서 남겨놓음
 }
