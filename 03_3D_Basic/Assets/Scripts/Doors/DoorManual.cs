560913c408434f1a11f55c17a411edba7419f82b
go2665 <77224762+go2665@users.noreply.github.com>
2023-03-21 13:21:30 +0900

    3D Basic - 사용 글자 표시 + 아이템 사용하는 스위치 추가

--- 03_3D_Basic/Assets/Scripts/Doors/DoorManual.cs
+++ 03_3D_Basic/Assets/Scripts/Doors/DoorManual.cs
@@ -1,11 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DoorManual : DoorBase, IUseableObject
 {
+    /// <summary>
+    /// 자동으로 닫힐때까지 걸리는 시간
+    /// </summary>
+    public float closeTime = 3.0f;
+
+    /// <summary>
+    /// 코루틴용으로 한번만 만들고 재활용하기 위한 용도
+    /// </summary>
+    WaitForSeconds closeWait;
+
+    TextMeshPro text;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        text = GetComponentInChildren<TextMeshPro>();
+    }
+
+    void Start()
+    {
+        text.gameObject.SetActive(false);
+        closeWait = new WaitForSeconds(closeTime); // 시작할 때 한번만 만들기 
+    }
+
+    /// <summary>
+    /// 이 오브젝트가 실행되는 함수
+    /// </summary>
     public void Used()
     {
-        Debug.Log("사용됨");
+        //Debug.Log("사용됨");
+        OnOpen();                       // 문 열기
+        StartCoroutine(AutoClose());    // closeTime초 이후에 자동으로 닫히게 하기
+    }
+
+    IEnumerator AutoClose()
+    {
+        yield return closeWait; // closeTime초 만큼 대기하고
+        OnClose();              // 문 닫기
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        text.gameObject.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        text.gameObject.SetActive(false);        
     }
 }
---
+++ 03_3D_Basic/Assets/Scripts/Doors/UseSwitch.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UseSwitch : MonoBehaviour, IUseableObject
+{
+    /// <summary>
+    /// 사용할 오브젝트
+    /// </summary>
+    public GameObject target;
+
+    /// <summary>
+    /// 사용할 오브젝트가 가지고 있는 IUseableObject 인터페이스
+    /// </summary>
+    IUseableObject useTarget;
+
+    /// <summary>
+    /// 사용중인지 표시하는 플래그
+    /// </summary>
+    bool isUsed = false;
+
+    Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        useTarget = target.GetComponent<IUseableObject>();  // 미리 찾아놓는 용도
+        if (useTarget == null)
+        {
+            Debug.LogWarning("사용할 수 없는 오브젝트가 설정되어 있습니다.");
+        }
+    }
+
+    /// <summary>
+    /// 이 오브젝트가 사용될 때 실행되는 함수
+    /// </summary>
+    public void Used()
+    {
+        if (useTarget != null)  // 사용할 대상이 있고
+        {
+            if (!isUsed)        // 사용중이 아니면
+            {
+                useTarget.Used();               // 대상을 사용하기
+                StartCoroutine(ResetSwitch());  // 코루틴으로 애니메이션 처리
+            }
+        }
+    }
+
+    IEnumerator ResetSwitch()
+    {
+        isUsed = true;                          // 사용중이라고 표시
+        anim.SetBool("IsOpen", true);           // 사용하는 애니메이션 실행
+        yield return new WaitForSeconds(1);     // 1초 기다리기
+        anim.SetBool("IsOpen", false);          // 원상태로 되돌리는 애니메이션 실행
+        isUsed = false;                         // 사용이 끝났다고 표시
+    }
+}
