f6274ca2a566b25a48886506a13387b83d128a40
go2665 <77224762+go2665@users.noreply.github.com>
2023-04-07 10:01:05 +0900

    Tilemap - Node 클래스 비교 처리 추가

--- 04_Tilemap/Assets/Scripts/Character/AStar/Node.cs
+++ 04_Tilemap/Assets/Scripts/Character/AStar/Node.cs
@@ -1,8 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Node
+public class Node : IComparable<Node>   // Node 클래스로 비교가 가능하게끔 IComparable 상속
 {
     // 축 기준 : 왼쪽 아래가 원점
     // 축 방향 : 오른쪽 x+, 위쪽 y+
@@ -51,4 +52,47 @@ public class Node
     /// 노드의 종류
     /// </summary>
     public GridType gridType = GridType.Plain;
+
+    /// <summary>
+    /// 크기 비교를 하기 위해서 추가된 함수
+    /// IComparable를 상속받았기 때문에 반드시 구현해야 한다.
+    /// </summary>
+    /// <param name="other">비교 대상</param>
+    /// <returns></returns>
+    public int CompareTo(Node other)
+    {
+        // 리턴이 0보다 작다     : 내가 작다(this < other)
+        // 리턴이 0이다          : this와 other가 크기(순서)가 같다.
+        // 리턴이 0보다 크다     : 내가 크다(this > other)
+
+        if ( other == null )            // other가 null이면 무조건 내가 크다.
+            return 1;
+
+        return F.CompareTo(other.F);    // F값을 기준으로 크기를 정하기
+    }
+
+    // == 연산자를 오버라이드 하기 위해 필요한 함수들 ------------------------------------------------
+    public override bool Equals(object obj)
+    {
+        // obj가 Node 타입이고 임시적으로 other라고 부름
+        // this와 other의 x가 같고 y도 같다.
+        return obj is Node other && this.x == other.x && this.y == other.y;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(this.x, this.y);    // x와 y를 기준으로 해시코드 생성
+    }    
+
+    // == 연산자는 Node와 Vector2Int로 비교
+    public static bool operator ==(Node left, Vector2Int right)
+    {
+        return left.x == right.x && left.y == right.y;  // x와 y 둘 다 같으면 같은 것으로 처리
+    }
+
+    // != 연산자는 Node와 Vector2Int로 비교
+    public static bool operator !=(Node left, Vector2Int right)
+    {
+        return left.x != right.x || left.y != right.y;  // x와 y 중 하나만 달라도 다른 것으로 처리
+    }
 }
